159d9a7ef6ab1f36613b4b67040aa7a866c11f86
nods21 <noah.disch@gmail.com>
2025-03-22 11:17:15 +0100

    PIC broad structure

--- src/main/PIC.java
+++ src/main/PIC.java
@@ -1,4 +1,29 @@
 package main;
 
+import java.util.ArrayList;
+
 public class PIC {
+    RAM bank0;
+    RAM bank1;
+    int wRegister;
+    int PCL;
+    ArrayList<Integer> program = new ArrayList<>();
+
+
+    public PIC() {
+        bank0 = new RAM();
+        bank1 = new RAM();
+        wRegister = 0;
+        PCL = 0;
+
+        //TODO Link to IO Pins
+        //TODO Link to Instruction Decoder
+        //TODO Stack init
+        //TODO: get program from Instruction Parser
+
+
+
+    }
+
+
 }
