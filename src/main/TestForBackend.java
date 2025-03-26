57b1013f32e12ea8f7fd198e9df19a6a5542968d
eddywayz <eduard.wayz@gmail.com>
2025-03-26 10:50:56 +0100

    TestForBackend.java with absolute pathes

--- src/main/TestForBackend.java
+++ src/main/TestForBackend.java
@@ -5,7 +5,17 @@ import java.util.ArrayList;
 
 public class TestForBackend {
     public static void main(String[] args) {
-        String path = "test_files\\TPicSim1.LST";
+        String path = "";
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("win")) {
+            path = "Dein/Weg/test_files/TPicSim1.LST";
+            System.out.println("Running on Windows");
+        } else if (osName.contains("mac")) {
+            path = "/Users/eddywayz/Desktop/Studium/Rechnerarchitektur/test_files/TPicSim1.LST";
+            System.out.println("Running on Mac");
+        } else {
+            System.out.println("Running on another OS: " + osName);
+        }
 
         FileLineParser flParser = new FileLineParser(path);
         flParser.printFile();
@@ -15,7 +25,6 @@ public class TestForBackend {
         for (Integer i : list) {
             System.out.println(Integer.toHexString(i.intValue()));
         }
-
 
     }
 }
