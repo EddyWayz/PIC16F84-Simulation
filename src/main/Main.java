b23f5cf3b97194b1e7c24b65f3388acbf3decf4a
eddywayz <eduard.wayz@gmail.com>
2025-04-03 12:17:49 +0200

    Change the main window to fill the full screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,34 +2,38 @@ package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.util.ArrayList;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("../resources/Main.fxml"));
-        primaryStage.setTitle("PIC16F84-Simulation");
-        primaryStage.setScene(new Scene(root, 1800, 1200));
-        primaryStage.show();
 
-/*
-        InstructionParser instParser = new InstructionParser(path);
-        ArrayList<Integer> list = instParser.parseLinesToInstructions();
-        for (Integer i : list) {
-            System.out.println(Integer.toHexString(i.intValue()));
-        }
-        */
+        // Hole die Abmessungen des primären Bildschirms
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+
+        // Erstelle eine Scene, die den gesamten Bildschirm ausfüllt
+        Scene scene = new Scene(root, screenBounds.getWidth(), screenBounds.getHeight());
+        primaryStage.setTitle("PIC16F84-Simulation");
+        primaryStage.setScene(scene);
+
+        // Setze die Fensterposition und -größe so, dass sie 100% des Bildschirms einnimmt
+        primaryStage.setX(screenBounds.getMinX());
+        primaryStage.setY(screenBounds.getMinY());
+        primaryStage.setWidth(screenBounds.getWidth());
+        primaryStage.setHeight(screenBounds.getHeight());
+
+        // primaryStage.setFullScreen(true);
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
-
     }
 }
