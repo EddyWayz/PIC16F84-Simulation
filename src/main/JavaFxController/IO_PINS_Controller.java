7187a74d65003f8a3faf6d4aea09f2040e1c0cac
eddywayz <eduard.wayz@gmail.com>
2025-04-24 10:57:58 +0200

    Correkt IO Pins

--- src/main/JavaFxController/IO_PINS_Controller.java
+++ src/main/JavaFxController/IO_PINS_Controller.java
@@ -38,31 +38,100 @@ public class IO_PINS_Controller {
 
         int size = pic.PortA.ioPins.length;
 
-        for (int row = 0; row < size; row++) {
-            for (int col = 0; col < size; col++) {
-                boolean value = pic.PortA.ioPins[row][col].getValue();
-                Label label = new Label(value ? "1" : "0");
+        // RA header labels (RA7 to RA0)
+        for (int i = 0; i < size; i++) {
+            int pin = size - 1 - i;
+            Label header = new Label("RA" + pin);
+            header.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            header.prefWidthProperty().bind(header.heightProperty());
+            GridPane.setHgrow(header, Priority.ALWAYS);
+            GridPane.setVgrow(header, Priority.ALWAYS);
+            header.setStyle(
+                "-fx-alignment: center;" +
+                "-fx-font-weight: bold;" +
+                "-fx-border-color: black;"
+            );
+            gridPane.add(header, i + 1, 0);
+        }
 
-                // Zelle komplett ausfüllen lassen
-                label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                label.prefWidthProperty().bind(label.heightProperty());
-                GridPane.setHgrow(label, Priority.ALWAYS);
-                GridPane.setVgrow(label, Priority.ALWAYS);
+        // Port A row
+        Label portALabel = new Label("Port A");
+        portALabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        GridPane.setHgrow(portALabel, Priority.ALWAYS);
+        GridPane.setVgrow(portALabel, Priority.ALWAYS);
+        portALabel.setStyle(
+            "-fx-alignment: center;" +
+            "-fx-font-weight: bold;" +
+            "-fx-border-color: black;" +
+            "-fx-pref-width: 80px;"
+        );
+        gridPane.add(portALabel, 0, 1);
+        for (int i = 0; i < size; i++) {
+            boolean value = pic.PortA.ioPins[i].getValue();
+            Label label = new Label(value ? "1" : "0");
+            label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            label.prefWidthProperty().bind(label.heightProperty());
+            GridPane.setHgrow(label, Priority.ALWAYS);
+            GridPane.setVgrow(label, Priority.ALWAYS);
+            label.setStyle(
+                "-fx-alignment: center;" +
+                "-fx-border-color: black;" +
+                "-fx-font-size: 14;"
+            );
+            final int index = i;
+            label.setOnMouseClicked(event -> {
+                pic.PortA.ioPins[index].toggleValue();
+                label.setText(pic.PortA.ioPins[index].getValue() ? "1" : "0");
+            });
+            gridPane.add(label, i + 1, 1);
+        }
 
-                label.setStyle(
-                        "-fx-alignment: center;" +
-                                "-fx-border-color: black;" +
-                                "-fx-font-size: 14;"
-                );
+        // RB header labels (RB7 to RB0)
+        for (int i = 0; i < size; i++) {
+            int pin = size - 1 - i;
+            Label header = new Label("RB" + pin);
+            header.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            header.prefWidthProperty().bind(header.heightProperty());
+            GridPane.setHgrow(header, Priority.ALWAYS);
+            GridPane.setVgrow(header, Priority.ALWAYS);
+            header.setStyle(
+                "-fx-alignment: center;" +
+                "-fx-font-weight: bold;" +
+                "-fx-border-color: black;"
+            );
+            gridPane.add(header, i + 1, 2);
+        }
 
-                final int r = row, c = col;
-                label.setOnMouseClicked(event -> {
-                    pic.PortA.ioPins[r][c].toggleValue();
-                    label.setText(pic.PortA.ioPins[r][c].getValue() ? "1" : "0");
-                });
-
-                gridPane.add(label, col, row);
-            }
+        // Port B row
+        Label portBLabel = new Label("Port B");
+        portBLabel.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        GridPane.setHgrow(portBLabel, Priority.ALWAYS);
+        GridPane.setVgrow(portBLabel, Priority.ALWAYS);
+        portBLabel.setStyle(
+            "-fx-alignment: center;" +
+            "-fx-font-weight: bold;" +
+            "-fx-border-color: black;" +
+            "-fx-pref-width: 80px;"
+        );
+        gridPane.add(portBLabel, 0, 3);
+        for (int i = 0; i < size; i++) {
+            boolean value = pic.PortB.ioPins[i].getValue();
+            Label label = new Label(value ? "1" : "0");
+            label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            label.prefWidthProperty().bind(label.heightProperty());
+            GridPane.setHgrow(label, Priority.ALWAYS);
+            GridPane.setVgrow(label, Priority.ALWAYS);
+            label.setStyle(
+                "-fx-alignment: center;" +
+                "-fx-border-color: black;" +
+                "-fx-font-size: 14;"
+            );
+            final int index = i;
+            label.setOnMouseClicked(event -> {
+                pic.PortB.ioPins[index].toggleValue();
+                label.setText(pic.PortB.ioPins[index].getValue() ? "1" : "0");
+            });
+            gridPane.add(label, i + 1, 3);
         }
     }
 }
