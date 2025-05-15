e2a10e72532b5c48961a152f09b2889e39265802
nods21 <noah.disch@gmail.com>
2025-05-15 12:11:12 +0200

    Testfile 5 läuft und Program 9 auch

--- src/main/timers/WatchdogTMR.java
+++ src/main/timers/WatchdogTMR.java
@@ -10,6 +10,10 @@ public class WatchdogTMR {
     Prescaler ps;
     int counter = 0;
     public boolean active = false;
+
+    //just for testing purposes
+    private int WDT_threshold = 50; //18 * (4 / quarz_freqency)
+
     public WatchdogTMR(PIC pic, Prescaler ps) {
         this.pic = pic;
         this.ps = ps;
@@ -22,11 +26,14 @@ public class WatchdogTMR {
         if (!active) {
             return;
         }
+
         ps.impulsFromWDT();
-        //TODO: connection to GUI to check if WDT is activated
+
         //18.000 instruction equals 18ms
-        if (counter >= 18_000) {
+        if (counter >= WDT_threshold) {
+            counter = 0;
             //reset or wake up depending on if the pic is in sleep mode
+            System.out.println("Reset from WDT");
             if (pic.getSleep()) {
                 pic.wakeUp_WDT();
             } else {
